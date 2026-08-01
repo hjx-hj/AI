e6f848f06c7714c524a91ef5fbd5092853f1d44d
黄佳星 <1741375382@qq.com>
2026-08-01 19:02:30 +0800

    Updates

--- assets/NotFound-BDR7TkpC.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-CMWUS37A.js";import{d as t}from"./vendor-C8NcrucJ.js";import{_ as r,B as o,$ as i}from"./antd-B6R02BHh.js";import"./utils-CkuJ2-VY.js";const a=()=>{const a=t();return s.jsx(r,{status:"403",title:"无权限访问",subTitle:"抱歉，您没有权限访问该页面",extra:s.jsx(o,{type:"primary",icon:s.jsx(i,{}),onClick:()=>a("/"),children:"返回首页"})})};export{a as NotFound};
